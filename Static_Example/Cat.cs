18fa7a2541de9c4bc8033e1539f4381e5abac139
Jarryd Huntley <Jarryd Huntley>
2017-10-12 16:58:55 -0400

    initial commit

---
+++ Static_Example/Cat.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Static_Example
+{
+    class Cat
+    {
+
+        private int catCount;
+
+        public int CatCount
+        {
+            get { return catCount; }
+            set { catCount = value; }  
+        }
+
+        public Cat()
+        {
+  
+        }
+
+        public void WarCry()
+        {
+            Console.WriteLine("We are an army of " + catCount + " cats!!");
+        }
+    }
+}
---
+++ Static_Example/Turtle.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Static_Example
+{
+    class Turtle
+    {
+        //This static variable belongs to the class. It's shared among all instances.
+        //This also means we can use the variable without creating any instances firt.
+        static int turtleCount;
+
+        public Turtle()
+        {
+            turtleCount++;
+        }
+
+
+        //This static method belongs to the class. It's shared among all instances.
+        //It means we can use the method without creating any instances firt.
+        public static void WarCry()
+        {
+            Console.WriteLine("We are an army of " + turtleCount + " turtles!!");
+        }
+    }
+}
